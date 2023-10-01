1fa8a16a31664aa14008999b9d1ad0ad51ad99b9
Sheneeka Ward <sheneeka.ward@thoughtworks.com>
2023-10-01 12:57:36 -0400

    [Sheneeka] Re-commenting out assume role

--- versions.tf
+++ versions.tf
@@ -21,10 +21,10 @@ provider "aws" {
   # this section commented out during the initial bootstrap run
   # once the assumeable roles are created, uncomment and change
   # secrethub.*.env to contain the appropriate service account identity
-  assume_role {
-    role_arn     = "arn:aws:iam::${var.aws_account_id}:role/${var.aws_account_role}"
-    session_name = "lab-iam-profiles"
-  }
+#  assume_role {
+#    role_arn     = "arn:aws:iam::${var.aws_account_id}:role/${var.aws_account_role}"
+#    session_name = "lab-iam-profiles"
+#  }
 
   default_tags {
     tags = {
