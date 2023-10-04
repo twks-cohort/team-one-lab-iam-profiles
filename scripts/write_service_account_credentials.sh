871589248a74cbe5b3e7f5a14459a8ea5f096dc5
Sheneeka Ward <sheneeka.ward@thoughtworks.com>
2023-10-04 00:51:04 -0400

    [Sheneeka] Comment out write service credentials - Might be clearing out 1password credential

--- scripts/write_service_account_credentials.sh
+++ scripts/write_service_account_credentials.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-terraform init
+#terraform init
 # secrethub run -- terraform output DPSSimpleServiceAccount_encrypted_aws_secret_access_key | base64 -d | gpg -dq --passphrase ${GPG_KEY_PASSPHRASE} | secrethub write twdps/di/svc/aws/dps-2/DPSSimpleServiceAccount/aws-secret-access-key
 # secrethub run -- terraform output DPSSimpleServiceAccount_aws_access_key_id | sed 's/"//g' | secrethub write twdps/di/svc/aws/dps-2/DPSSimpleServiceAccount/aws-access-key-id
 
 # write the non-prod service account credentials to secret store
-terraform output DPSNonprodServiceAccount_encrypted_aws_secret_access_key | sed 's/"//g' | base64 -d | gpg -dq --passphrase ${GPG_KEY_PASSPHRASE} |  opw write teamone-aws DPSNonprodServiceAccount-aws-secret-access-key -
-terraform output DPSNonprodServiceAccount_aws_access_key_id | tr -d \\n | sed 's/"//g' | opw write teamone-aws DPSNonprodServiceAccount-aws-access-key-id -
+#terraform output DPSNonprodServiceAccount_encrypted_aws_secret_access_key | sed 's/"//g' | base64 -d | gpg -dq --passphrase ${GPG_KEY_PASSPHRASE} |  opw write teamone-aws DPSNonprodServiceAccount-aws-secret-access-key -
+#terraform output DPSNonprodServiceAccount_aws_access_key_id | tr -d \\n | sed 's/"//g' | opw write teamone-aws DPSNonprodServiceAccount-aws-access-key-id -
